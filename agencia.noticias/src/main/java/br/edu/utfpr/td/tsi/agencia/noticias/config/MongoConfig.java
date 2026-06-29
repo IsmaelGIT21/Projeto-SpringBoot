979534d784db873d6372f4e97ed186bb8b7f3c42
JoaoPedroBerwanger <joaopedroberwanger12@gmail.com>
2026-06-28 22:18:03 -0300

    Apresentacao

--- agencia.noticias/src/main/java/br/edu/utfpr/td/tsi/agencia/noticias/config/MongoConfig.java
+++ agencia.noticias/src/main/java/br/edu/utfpr/td/tsi/agencia/noticias/config/MongoConfig.java
@@ -1,6 +1,9 @@
 package br.edu.utfpr.td.tsi.agencia.noticias.config;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -11,6 +14,14 @@ public class MongoConfig {
 
 	private static final String NOME_BANCO = "agencia-noticias";
 
+	@Value("${spring.data.mongodb.uri}")
+	private String uri;
+	
+	@Bean
+	public MongoClient mongoClient() {
+		return MongoClients.create(uri);
+	}
+	
 	@Bean
 	public MongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
 		return new SimpleMongoClientDatabaseFactory(mongoClient, NOME_BANCO);
